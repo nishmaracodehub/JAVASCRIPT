8afdf85652eecbc304ac753c32d70c99febae5e8
nishmaracodehub <nishanthmaraboina@gmail.com>
2019-09-17 17:58:31 +0530

    silly story

---
+++ Silly-Story-Generator/JS/main.js
@@ -0,0 +1,54 @@
+// 1. COMPLETE VARIABLE AND FUNCTION DEFINITIONS
+
+var customName = document.getElementById("customname");
+var randomize = document.querySelector(".randomize");
+var story = document.querySelector(".story");
+
+function randomValueFromArray(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+// 2. RAW TEXT STRINGS
+
+let storyText =
+  "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+
+let insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
+let insertY = ["the soup kitchen", "Disneyland", "the White House"];
+let insertZ = [
+  "spontaneously combusted",
+  "melted into a puddle on the sidewalk",
+  "turned into a slug and crawled away"
+];
+
+// 3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION
+
+randomize.addEventListener("click", result);
+
+function result() {
+  let newStory = storyText;
+  let xItem = randomValueFromArray(insertX);
+  let yItem = randomValueFromArray(insertY);
+  let zItem = randomValueFromArray(insertZ);
+
+  newStory = newStory.replace(":insertx:", xItem);
+  newStory = newStory.replace(":insertx:", xItem);
+  newStory = newStory.replace(":inserty:", yItem);
+  newStory = newStory.replace(":insertz:", zItem);
+
+  if (customName.value !== "") {
+    var name = customName.value;
+    newStory = newStory.replace("Bob", name);
+  }
+
+  if (document.getElementById("uk").checked) {
+    var weight = Math.round(300 / 14) + " stones"; //converstion of pounds to stones(UK)
+    var temperature = Math.round((94 - 32) / 1.8) + " centigrade"; //conversion of fahrenheit to Centigrade
+
+    newStory = newStory.replace("94 fahrenheit", temperature);
+    newStory = newStory.replace("300 pounds", weight);
+  }
+
+  story.textContent = newStory;
+  story.style.visibility = "visible";
+}
